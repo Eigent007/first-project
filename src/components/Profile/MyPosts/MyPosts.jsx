d31516fb6181e629b99911f9133e427bd65728ec
Azat <azat1hajy@gmail.com>
2022-06-15 17:14:11 +0300

    Update MyPosts.jsx

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
-import { addPostActionCreator, AddPostTextActionCreator } from "../../../redux/profile-reducer";
 
 const MyPosts = (props) => {
   let postElements = props.posts.map((postsData) => (
@@ -14,19 +13,19 @@ const MyPosts = (props) => {
 
   let newPostElement = React.createRef();
 
-  let addPost = () => {
-    //props.addPost();
-    //props.updatePostText(' ');
-    props.dispatch(addPostActionCreator());
+  let onAddPost = () => {
+    props.addPost();
+    props.updatePostText(' ');
+    //props.dispatch(addPostActionCreator());
     //props.dispatch({type:'Add-Post-Text'});
   };
- 
 
   let onPostChange = () => {
     let newText = newPostElement.current.value;
-    //props.updatePostText(newText);
+    props.updatePostText(newText);
     //let action={type:'Add-Post-Text', newInputtedText:newText};
-    props.dispatch(AddPostTextActionCreator(newText));
+    //props.dispatch(AddPostTextActionCreator(newText));
+    //AddPostTextActionCreator(newText);
   };
 
   return (
@@ -40,7 +39,7 @@ const MyPosts = (props) => {
         />
       </div>
       <div>
-        <button onClick={addPost}>Add post</button>
+        <button onClick={onAddPost}>Add post</button>
       </div>
       <div className={s.posts}>{postElements}</div>
     </div>
