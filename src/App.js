1ff1658131b2b4099da51a4a5961bd218e39c8d0
Azat <azat1hajy@gmail.com>
2022-06-15 17:12:44 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Dialogs from "./components/Dialogs/Dialogs";
+import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 
 const App = (props) => {
- 
   return (
     <BrowserRouter>
       <div className="app-wrapper">
@@ -16,8 +16,8 @@ const App = (props) => {
 
         <div className="app-wrapper-content">
           <Routes>
-            <Route path="/profile" element={<Profile feedData={props.state.feedData} dispatch={props.dispatch} />}/>
-            <Route path="/dialogs" element={<Dialogs store = {props.store} />} />
+            <Route path="/profile" element={<Profile store={props.store} />} />
+            <Route path="/dialogs" element={<DialogsContainer store={props.store} />} />
           </Routes>
         </div>
       </div>
