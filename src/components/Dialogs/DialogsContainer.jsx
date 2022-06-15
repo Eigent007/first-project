3497fa8e1cb8a0783daa856505cc3535ef58ae1b
Azat <azat1hajy@gmail.com>
2022-06-16 00:10:34 +0300

    Update DialogsContainer.jsx

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -1,22 +1,37 @@
 import React from "react";
-import { sendMessageCreator, updateInputtedTextBodyCreator } from "../../redux/dialog-reducer";
+import {
+  sendMessageCreator,
+  updateInputtedTextBodyCreator,
+} from "../../redux/dialog-reducer";
+import StoreContext from "../../storeContext";
 import Dialogs from "./Dialogs";
 
 const DialogsContainer = (props) => {
   debugger;
-  
-  let state = props.store.getState().chatData;
-  let newMessageBody = state.newMessageBody;
+  return (
+    <StoreContext.Consumer>
+      {(store) => {
+        let state = store.getState().chatData;
+        let newMessageBody = state.newMessageBody;
 
-  let addMessage1 = () => {
-    props.store.dispatch(sendMessageCreator());
-  }
-  let onMessageChange1 = (body) => {
-    //let body = e.target.value;
-    props.store.dispatch(updateInputtedTextBodyCreator(body));
-  }
-  
-  return (<Dialogs onMessageChange={onMessageChange1} addMessage={addMessage1} state={state} newMessageBody={newMessageBody} />
+        let addMessage1 = () => {
+          store.dispatch(sendMessageCreator());
+        };
+
+        let onMessageChange1 = (body) => {
+          //let body = e.target.value;
+          store.dispatch(updateInputtedTextBodyCreator(body));
+        };
+        return (
+          <Dialogs
+            onMessageChange={onMessageChange1}
+            addMessage={addMessage1}
+            state={state}
+            newMessageBody={newMessageBody}
+          />
+        );
+      }}
+    </StoreContext.Consumer>
   );
 };
 
