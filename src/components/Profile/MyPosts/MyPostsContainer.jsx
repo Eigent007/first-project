44d20ec88621f603f65fbc4516feadd479f891da
Azat <azat1hajy@gmail.com>
2022-06-16 00:11:38 +0300

    Update MyPostsContainer.jsx

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -3,32 +3,38 @@ import {
   addPostActionCreator,
   AddPostTextActionCreator,
 } from "../../../redux/profile-reducer";
+import StoreContext from "../../../storeContext";
 import MyPosts from "./MyPosts";
 
 const MyPostsContainer = (props) => {
-  let state = props.store.getState();
-
-  let addPost = () => {
-    //props.addPost();
-    //props.updatePostText(' ');
-    props.store.dispatch(addPostActionCreator());
-    //props.dispatch({type:'Add-Post-Text'});
-  };
-
-  let onPostChange = (newText) => {
-    //let newText = newPostElement.current.value;
-    //props.updatePostText(newText);
-    //let action={type:'Add-Post-Text', newInputtedText:newText};
-    props.store.dispatch(AddPostTextActionCreator(newText));
-  };
-
   return (
-    <MyPosts
-      updatePostText={onPostChange}
-      addPost={addPost}
-      posts={state.feedData.posts}
-      newPostText={state.feedData.newPostText}
-    />
+    <StoreContext.Consumer>
+      {(store) => {
+        let state = store.getState();
+
+        let addPost = () => {
+          //props.addPost();
+          //props.updatePostText(' ');
+          store.dispatch(addPostActionCreator());
+          //props.dispatch({type:'Add-Post-Text'});
+        };
+
+        let onPostChange = (newText) => {
+          //let newText = newPostElement.current.value;
+          //props.updatePostText(newText);
+          //let action={type:'Add-Post-Text', newInputtedText:newText};
+          store.dispatch(AddPostTextActionCreator(newText));
+        };
+        return (
+          <MyPosts
+            updatePostText={onPostChange}
+            addPost={addPost}
+            posts={state.feedData.posts}
+            newPostText={state.feedData.newPostText}
+          />
+        );
+      }}
+    </StoreContext.Consumer>
   );
 };
 
