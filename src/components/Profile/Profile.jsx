acf98083d3b1b9c3616a88665d2ad2fbb589b7b5
Azat <azat1hajy@gmail.com>
2022-06-15 17:13:23 +0300

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,16 +1,13 @@
 import React from "react";
-import MyPosts from "./MyPosts/MyPosts";
 import ProfileInfo from "../ProfileInfo/ProfileInfo";
+import MyPostsContainer from "./MyPosts/MyPostsContainer";
 
 const Profile = (props) => {
   debugger;
   return (
     <div>
       <ProfileInfo />
-      <MyPosts
-        posts={props.feedData.posts}
-        newPostText={props.feedData.newPostText}
-        dispatch={props.dispatch}
+      <MyPostsContainer store={props.store}
       />
     </div>
   );
