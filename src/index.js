39d80d3d1a821aa7c177d27976f1973d1a5613ae
Azat <azat1hajy@gmail.com>
2022-06-17 21:28:00 +0300

    47 lesson Profile Reducer copy

--- src/index.js
+++ src/index.js
@@ -7,8 +7,7 @@ import App from "./App";
 import { Provider } from "react-redux";
 
 
-let rerenderAllTree = () => {
-  debugger;
+
   const root = ReactDOM.createRoot(document.getElementById("root"));
   root.render(
     <React.StrictMode>
@@ -17,15 +16,7 @@ let rerenderAllTree = () => {
       </Provider>
     </React.StrictMode>
   );
-};
-
-rerenderAllTree();
-
-store.subscribe(() => {
-  let state = store.getState();
-  rerenderAllTree(state);
-});
-
+ 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -2,15 +2,15 @@ const Add_Post = "Add-Post";
 const Add_Post_Text = "Add-Post-Text";
 
 let initialState = {
-    posts: [
-      { id: 1, name: "firstpost", likeCount: 20 },  
-      { id: 2, name: "secondpost", likeCount: 11 },
-    ],
-    newPostText: "agent 47",
+  posts: [
+    { id: 1, name: "firstpost", likeCount: 20 },
+    { id: 2, name: "secondpost", likeCount: 11 },
+  ],
+  newPostText: "agent 47",
 };
 
 const profileReducer = (state = initialState, action) => {
-    debugger;
+  debugger;
   switch (action.type) {
     case Add_Post:
       let mess_add_helper = {
@@ -18,22 +18,35 @@ const profileReducer = (state = initialState, action) => {
         name: state.newPostText,
         likeCount: 20,
       };
-      state.posts.push(mess_add_helper);
-      state.newPostText = "";
-      return state;
+      let stateCopy = { ...state };
+      stateCopy.posts = [...state.posts];
+      stateCopy.posts.push(mess_add_helper);
+      stateCopy.newPostText = { ...state.newPostText };
+      stateCopy.newPostText = "";
+      return stateCopy;
+    //       state.posts.push(mess_add_helper);
+    //       state.newPostText = "";
+    //       return state;
 
-    case Add_Post_Text:
-      state.newPostText = action.newInputtedText;
-      return state;
+    case Add_Post_Text: {
+      let stateCopy = { ...state };
+      stateCopy.posts = [...state.posts];
+      stateCopy.newPostText = action.newInputtedText;
+      return stateCopy;
+    }
+
+    // state.newPostText = action.newInputtedText;
+    //   return state;
 
     default:
       return state;
   }
 };
 
-export const addPostActionCreator = () => ({type: Add_Post })
-export const AddPostTextActionCreator = (newText) => 
-({ type: Add_Post_Text, newInputtedText: newText })
-
+export const addPostActionCreator = () => ({ type: Add_Post });
+export const AddPostTextActionCreator = (newText) => ({
+  type: Add_Post_Text,
+  newInputtedText: newText,
+});
 
 export default profileReducer;
